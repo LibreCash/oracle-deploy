6c7a6c17e5a6c3c678cbe74d912bbe6d8fa5e5f4
decodemybrain <decodemybrain@protonmail.ch>
2018-02-12 08:45:15 +0300

    Delete blank script

--- install-systemd.js
+++
@@ -1,7 +0,0 @@
-// systemd installer prototype
-
-// commands: install <geth|lightnode|masternode|ui>
-// - geth <network> <port> <options>
-// - lightnode <config>
-// - masternode <config>
-// - ui <config>
